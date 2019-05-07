267fc509e2b5a19f77ceab4254d9ff0b74444f6c
Alan Tai <alan.tai@zuhlke.com>
2019-05-07 13:24:54 +0800

    Fix news query by news publisher names

--- src/main/java/com/github/ayltai/hknews/controller/ItemController.java
+++ src/main/java/com/github/ayltai/hknews/controller/ItemController.java
@@ -1,5 +1,6 @@
 package com.github.ayltai.hknews.controller;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.ayltai.hknews.data.model.Item;
+import com.github.ayltai.hknews.data.model.Source;
 import com.github.ayltai.hknews.data.repository.ItemRepository;
 
 @RestController
@@ -28,6 +30,9 @@ public final class ItemController {
         produces = "application/json"
     )
     public Iterable<Item> getItems(@NonNull @lombok.NonNull @PathVariable final List<String> sourceNames, @NonNull @lombok.NonNull @PathVariable final List<String> categoryNames, @PathVariable final int days) {
+        final List<String> names = new ArrayList<>();
+        for (final String sourceName : sourceNames) names.addAll(Source.fromDisplayName(sourceName));
+
         final Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, 0);
         calendar.set(Calendar.MINUTE, 0);
@@ -35,6 +40,6 @@ public final class ItemController {
         calendar.set(Calendar.MILLISECOND, 0);
         calendar.add(Calendar.DATE, -days);
 
-        return this.itemRepository.findBySourceInAndCategoryNameInAndPublishDateAfter(sourceNames, categoryNames, calendar.getTime(), Sort.by(Sort.Direction.DESC, Item.FIELD_PUBLISH_DATE));
+        return this.itemRepository.findBySourceInAndCategoryNameInAndPublishDateAfter(names, categoryNames, calendar.getTime(), Sort.by(Sort.Direction.DESC, Item.FIELD_PUBLISH_DATE));
     }
 }
