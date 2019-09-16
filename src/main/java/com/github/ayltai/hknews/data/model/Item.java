18e885625463d9195abdc3f970036388ef2fa8c2
Alan Tai <alan.tai@zuhlke.com>
2019-09-16 15:19:06 +0800

    Update item query and re-enable text index

--- src/main/java/com/github/ayltai/hknews/data/model/Item.java
+++ src/main/java/com/github/ayltai/hknews/data/model/Item.java
@@ -8,6 +8,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.IndexDirection;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.index.TextIndexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -34,10 +35,12 @@ public final class Item {
 
     @Getter
     @Setter
+    @TextIndexed
     private String title;
 
     @Getter
     @Setter
+    @TextIndexed
     private String description;
 
     @EqualsAndHashCode.Include
