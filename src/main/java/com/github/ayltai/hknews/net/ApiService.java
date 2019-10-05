859bc0f77e142150f097483078af106ff1461f4e
Alan Tai <alan.tai@zuhlke.com>
2019-10-05 17:33:19 +0800

    Remove Prometheus integration

--- src/main/java/com/github/ayltai/hknews/net/ApiService.java
+++ src/main/java/com/github/ayltai/hknews/net/ApiService.java
@@ -4,7 +4,6 @@ import org.springframework.lang.NonNull;
 
 import com.github.ayltai.hknews.rss.Feed;
 
-import io.micrometer.core.annotation.Timed;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
@@ -14,17 +13,14 @@ import retrofit2.http.Url;
 
 public interface ApiService {
     @NonNull
-    @Timed("ext_get_feed")
     @GET
     Call<Feed> getFeed(@Url String url);
 
     @NonNull
-    @Timed("ext_get_html")
     @GET
     Call<String> getHtml(@Url String url);
 
     @NonNull
-    @Timed("ext_post_html")
     @FormUrlEncoded
     @POST
     Call<String> postHtml(@Url String url, @Field("sid") int sectionId, @Field("p") int page);
